179260d2c79e381f39a942a0acc5d8abd8527ffb
N-A-V-I-D <NC1939@nyu.edu>
2021-10-01 01:35:33 -0400

    Unit 02 Assignment
    Completed Unit 02 Assignment

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Navid Chowdhury on 10/1/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
